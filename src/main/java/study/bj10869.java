5d5bbf12efd266f934b8a4bd8f3dde8d16299616
windw <windworm119@gmail.com>
2023-04-09 10:56:15 +0900

    2023-04-09

---
+++ src/main/java/study/bj10869.java
@@ -0,0 +1,18 @@
+package study;
+
+import java.util.Scanner;
+
+public class bj10869 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
