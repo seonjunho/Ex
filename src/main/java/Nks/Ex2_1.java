31558b492a9fc0f269b59301dd3a5dfcf15c40bb
windw <windworm119@gmail.com>
2023-04-14 17:30:06 +0900

    2023-04-14

---
+++ src/main/java/Nks/Ex2_1.java
@@ -0,0 +1,17 @@
+package Nks;
+
+public class Ex2_1 {
+    public static void main(String[] args){
+        int year = 0;
+        int age = 14;
+
+        System.out.println(year);
+        System.out.println(age);
+
+        year = 2000;
+        age += 1;
+
+        System.out.println(year);
+        System.out.println(age);
+    }
+}
