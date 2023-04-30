8ed7d4e63a223835473e9561bee2729d04fc9986
windw <windworm119@gmail.com>
2023-04-30 13:43:28 +0900

    2023-04-30

---
+++ src/main/java/Ex09/Ex09_1.java
@@ -0,0 +1,13 @@
+package Ex09;
+
+public class Ex09_1 {
+    public static void main(String[] args) {
+        String str = "IT Cookbook. Java";
+        int len;
+
+        len = str.length();
+
+        System.out.printf("문자열 :%s \n", str);
+        System.out.printf("문자열 길이 : %d \n", len);
+    }
+}
