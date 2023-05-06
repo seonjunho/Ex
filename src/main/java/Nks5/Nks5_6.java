2b1f41117cc9f7587c231be66f29a9ab8f5086cc
windw <windworm119@gmail.com>
2023-05-06 17:59:01 +0900

    2023-05-06

---
+++ src/main/java/Nks5/Nks5_6.java
@@ -0,0 +1,20 @@
+package Nks5;
+
+public class Nks5_6 {
+    public static void main(String[] args) {
+        int[] score = {79, 88, 91, 33, 100, 55, 95};
+
+        int max = score[0];
+        int min = score[0];
+
+        for (int i = 0; i < score.length; i++) {
+            if (max < score[i]) {
+                max = score[i];
+            } else if (score[i] < min) {
+                min = score[i];
+            }
+        }
+        System.out.println("최대값 : "+max);
+        System.out.println("최소값 : "+min);
+    }
+}
