51beb620f32ac4acf37c381257c8740236efbeb5
windw <windworm119@gmail.com>
2023-05-10 14:45:22 +0900

    2023-05-10

---
+++ src/main/java/Nks5/Nks5_10.java
@@ -0,0 +1,30 @@
+package Nks5;
+
+public class Nks5_10 {
+    public static void main(String[] args) {
+        int[] numArr = new int[10];
+
+        for(int i = 0; i < numArr.length; i++){
+            System.out.print(numArr[i] = (int)(Math.random() * 10));
+        }
+        System.out.println();
+
+        for (int i = 0 ; i< numArr.length-1 ; i++){
+            boolean changed = false;
+
+            for (int j=0; j < numArr.length-1-i;j++){
+                if(numArr[j] > numArr[j+1]){ //numArr[j]와 바로 옆 numArr[j+1]을 비교
+                    int tmp = numArr[j];
+                    numArr[j] = numArr[j+1];
+                    numArr[j+1] = tmp;
+                    changed = true;
+                }
+            }
+            if (!changed) break;
+
+            for(int k=0; k<numArr.length;k++)
+                System.out.print(numArr[k]);
+            System.out.println();
+        }
+    }
+}
