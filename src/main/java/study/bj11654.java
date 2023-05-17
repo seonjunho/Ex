bbbf210299014e43a894a31f192014827a78c6b9
windw <windworm119@gmail.com>
2023-05-18 06:50:41 +0900

    2023-05-18

---
+++ src/main/java/study/bj11654.java
@@ -0,0 +1,13 @@
+package study;
+
+import java.util.Scanner;
+
+public class bj11654 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int ch = in.next().charAt(0);
+
+        System.out.println(ch);
+    }
+}
