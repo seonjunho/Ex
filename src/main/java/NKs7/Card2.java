b1575ebbe192bea0f156bec261417a10c3831819
windw <windworm119@gmail.com>
2023-06-16 07:01:19 +0900

    2023-06-16

---
+++ src/main/java/NKs7/Card2.java
@@ -0,0 +1,31 @@
+package NKs7;
+
+class Card2 {
+    final int NUMBER;
+    final String KIND;
+    static int width = 100;
+    static int height = 250;
+
+    Card2(String kind, int num){
+        KIND = kind;
+        NUMBER = num;
+    }
+
+    Card2(){
+        this("HEART", 1);
+    }
+
+    public String toString(){
+        return KIND +" "+ NUMBER;
+    }
+}
+
+class FinalCardTest{
+    public static void main(String args[]) {
+        Card2 c = new Card2("HERAT",10);
+//        c.NUMBER = 5; // cannot assign a value to final variable NUMBER
+        System.out.println(c.KIND);
+        System.out.println(c.NUMBER);
+        System.out.println(c);
+    }
+}
