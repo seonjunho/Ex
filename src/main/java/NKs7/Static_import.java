b7699d03d8e7b3c9da8f8436e64e21aada7af6c5
windw <windworm119@gmail.com>
2023-07-02 07:11:18 +0900

    2023-07-02

---
+++ src/main/java/NKs7/Static_import.java
@@ -0,0 +1,12 @@
+package NKs7;
+import static java.lang.System.out;
+import static java.lang.Math.*;
+
+public class Static_import {
+    public static void main(String[] args) {
+
+        out.println(random());
+        out.println("Math.PI :"+ PI);
+
+    }
+}
