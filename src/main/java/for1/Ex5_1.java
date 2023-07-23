492f9c76ae8190c9d3da78a11fddd8133ca5fbe1
windw <windworm119@gmail.com>
2023-07-23 10:07:55 +0900

    2023-07-23

---
+++ src/main/java/for1/Ex5_1.java
@@ -0,0 +1,13 @@
+package for1;
+
+public class Ex5_1 {
+    public static void main(String[] args) {
+        for(int i = 0 ; i <= 5 ; i++)
+            System.out.println(i); // i값을 출력한다.
+
+        for(int i = 1 ; i <= 5 ; i++)
+            System.out.print(i); // print() 를 쓰면 가로롤 출력된다.
+
+        System.out.println();
+    }
+}
